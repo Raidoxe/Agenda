8bfc320b802320248817624f1536fdd8000acc46
Oliver Huth <olivernhuthpriority@gmail.com>
2021-07-20 18:09:10 +0800

    Created Feed component

---
+++ components/Feed.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import styles from '../Styling/FeedStyles';
+
+const Feed: React.FC<{}> = () => {
+
+
+    return (
+        <View style={styles.container}>
+            
+        </View>
+    )
+}
+
+export default Feed;
